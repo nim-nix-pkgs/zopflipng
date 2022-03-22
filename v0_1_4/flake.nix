b44dcc101d0b5fc49c71e01835905ae1966a2755
hugosenari <hugosenari@gmail.com>
2022-03-22 00:39:29 -0300

    chore: re-index ../nimpkgs/z/zopflipng

--- v0_1_4/flake.nix
+++ v0_1_4/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-zopflipng-v0_1_4.flake = false;
   inputs.src-zopflipng-v0_1_4.owner = "bung87";
-  inputs.src-zopflipng-v0_1_4.ref   = "refs/tags/v0.1.4";
+  inputs.src-zopflipng-v0_1_4.ref   = "v0_1_4";
   inputs.src-zopflipng-v0_1_4.repo  = "zopflipng";
   inputs.src-zopflipng-v0_1_4.type  = "github";
   
